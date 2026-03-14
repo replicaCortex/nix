3ca605cb2f1cb4a7b9174e31b496122f0d3082ba
replicaCortex <replicaCortex@gmail.com>
2026-03-14 21:40:39 +0300

    fixup

---
+++ nixos/system/network.nix
@@ -0,0 +1,5 @@
+{
+  networking.networkmanager.enable = true;
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+}
